ce900c57fe3f8c3aac97c33f27bfeb8d0b50c848
HBA32 <144519436+HBA32@users.noreply.github.com>
2023-09-23 17:26:20 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,24 @@
+class BankAccount:
+  def__init__(self, account_number,account_holder_name, initial_balance=0.0):
+  self. __account_number = account_number 
+self. __account_holder_name=account_holder_name 
+self. __account_balance=initial_balance
+def deposit (self, amount):
+  if amount>0:
+    self. __account_balance+=amount 
+print ("Deposit ₹{}.New balance:₹{}.format (amount, self. __account_balance"))
+else:
+print ("Invalid deposit amount, please deposit a positive amount")
+def withdraw(self, amount):
+  if amount>0 and amount<=self.__account_balance:
+    self__account__balance_=amount 
+print ("with drew₹{}.New balance:₹{}".format(amount,self__account=balance))
+else: 
+print ("Invalid withdrawal amount ot insufficient balance") 
+def display_balance (self): 
+print ("Account balance for{}(account#{}):₹{}".format(self.__account_holder_ne, self. __account_number, self. __account.balance)) 
+account=BankAccount (account_number="123456789",account_holder_name="Hari prabhu",initial_balance=5000.0)
+account. display_balance () 
+account. deposit (500.0)
+account. withdraw (200.0)
+account. display_balance ()
